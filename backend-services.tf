7b58865916aaa94b2ee1a0bc23b2799b789be1a1
Ced M <mouneac@gmail.com>
2023-12-19 19:19:56 -0600

    Bean app

--- backend-services.tf
+++ backend-services.tf
@@ -1,5 +1,5 @@
 resource "aws_db_subnet_group" "vpro-rds-subgrp" {
-  name = "main"
+  name       = "main"
   subnet_ids = [module.VPC.private_subnets[0], module.VPC.private_subnets[1], module.VPC.private_subnets[2]]
   tags = {
     Name = "Subnet group for RDS"
@@ -7,7 +7,7 @@ resource "aws_db_subnet_group" "vpro-rds-subgrp" {
 }
 
 resource "aws_elasticache_subnet_group" "vpro-ecache-subgrp" {
-  name = "vpro-ecache-subgrp"
+  name       = "vpro-ecache-subgrp"
   subnet_ids = [module.VPC.private_subnets[0], module.VPC.private_subnets[1], module.VPC.private_subnets[2]]
   tags = {
     Name = "Subnet group for ECACHE"
@@ -16,40 +16,40 @@ resource "aws_elasticache_subnet_group" "vpro-ecache-subgrp" {
 
 resource "aws_db_instance" "vpro-rds" {
   allocated_storage = 20
-  storage_type = "gp2"
-  engine = "mysql"
-  engine_version = "5.6.34"
-  instance_class = "db.t2.micro"
-#   name = var.dbname
-  db_name = var.dbname
-  username = var.dbuser
-  password = var.dbpass
-  parameter_group_name = "default.mysql5.6"
-  multi_az = "false"
-  publicly_accessible = "false"
-  skip_final_snapshot = true
-  db_subnet_group_name = aws_db_subnet_group.vpro-rds-subgrp.name
+  storage_type      = "gp2"
+  engine            = "mysql"
+  engine_version    = "5.6.34"
+  instance_class    = "db.t2.micro"
+  #   name = var.dbname
+  db_name                = var.dbname
+  username               = var.dbuser
+  password               = var.dbpass
+  parameter_group_name   = "default.mysql5.6"
+  multi_az               = "false"
+  publicly_accessible    = "false"
+  skip_final_snapshot    = true
+  db_subnet_group_name   = aws_db_subnet_group.vpro-rds-subgrp.name
   vpc_security_group_ids = [aws_security_group.vpro-backend-sg.id]
 }
 
 resource "aws_elasticache_cluster" "vpro-cache" {
-  cluster_id = "vpro-cache"
-  engine = "memcached"
-  node_type = "cache.t2.micro"
-  num_cache_nodes = 1
+  cluster_id           = "vpro-cache"
+  engine               = "memcached"
+  node_type            = "cache.t2.micro"
+  num_cache_nodes      = 1
   parameter_group_name = "default.memcached1.5"
-  port = 11211
-  security_group_ids = [aws_security_group.vpro-backend-sg.id]
-  subnet_group_name = aws_elasticache_subnet_group.vpro-ecache-subgrp.name
+  port                 = 11211
+  security_group_ids   = [aws_security_group.vpro-backend-sg.id]
+  subnet_group_name    = aws_elasticache_subnet_group.vpro-ecache-subgrp.name
 }
 
 resource "aws_mq_broker" "vpro-rmq" {
-  broker_name = "vpro-rmq"
-  engine_type = "ActiveMQ"
-  engine_version = "5.15.0"
+  broker_name        = "vpro-rmq"
+  engine_type        = "ActiveMQ"
+  engine_version     = "5.15.0"
   host_instance_type = "mq.t2.micro"
-  security_groups = [aws_security_group.vpro-backend-sg.id]
-  subnet_ids = [module.VPC.private_subnets[0]]
+  security_groups    = [aws_security_group.vpro-backend-sg.id]
+  subnet_ids         = [module.VPC.private_subnets[0]]
 
   user {
     username = var.rmquser
---
+++ bean-app.tf
@@ -0,0 +1,3 @@
+resource "aws_elastic_beanstalk_application" "vpro-prod" {
+  name = "vpro-prod"
+}
