5df3339701b5b806b246f194f15cb02a67e7efdc
Ced M <mouneac@gmail.com>
2023-12-19 21:49:16 -0600

    Improvement on bean-ev.tf

--- backend-services.tf
+++ backend-services.tf
@@ -18,13 +18,13 @@ resource "aws_db_instance" "vpro-rds" {
   allocated_storage = 20
   storage_type      = "gp2"
   engine            = "mysql"
-  engine_version    = "5.6.34"
+  engine_version    = "8.0.33"
   instance_class    = "db.t2.micro"
   #   name = var.dbname
   db_name                = var.dbname
   username               = var.dbuser
   password               = var.dbpass
-  parameter_group_name   = "default.mysql5.6"
+#   parameter_group_name   = "default.mysql5.7"
   multi_az               = "false"
   publicly_accessible    = "false"
   skip_final_snapshot    = true
@@ -37,7 +37,7 @@ resource "aws_elasticache_cluster" "vpro-cache" {
   engine               = "memcached"
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.memcached1.5"
+  parameter_group_name = "default.memcached1.6"
   port                 = 11211
   security_group_ids   = [aws_security_group.vpro-backend-sg.id]
   subnet_group_name    = aws_elasticache_subnet_group.vpro-ecache-subgrp.name
@@ -46,7 +46,7 @@ resource "aws_elasticache_cluster" "vpro-cache" {
 resource "aws_mq_broker" "vpro-rmq" {
   broker_name        = "vpro-rmq"
   engine_type        = "ActiveMQ"
-  engine_version     = "5.15.0"
+  engine_version     = "5.17.6"
   host_instance_type = "mq.t2.micro"
   security_groups    = [aws_security_group.vpro-backend-sg.id]
   subnet_ids         = [module.VPC.private_subnets[0]]
